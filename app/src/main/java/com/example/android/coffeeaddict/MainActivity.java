e8e0ca3fba28f787fca2901e8cc8e55a2aa4921b
daniel-enere <daniel.enere@gmail.com>
2017-10-29 17:45:36 -0500

    added text views and buttons

--- app/src/main/java/com/example/android/coffeeaddict/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeaddict/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.android.coffeeaddict;
 
+import android.annotation.TargetApi;
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int numCoffees = 2;
+        display(numCoffees);
+        displayPrice(numCoffees*3);
+    }
+
+    public void increaseOrder(View view) {
+        int increaseQuantity = 3;
+        increaseQuantity = 4;
+        display(increaseQuantity);
+    }
+
+    public void decreaseOrder(View view) {
+        int decreaseQuantity = 1;
+        decreaseQuantity = 0;
+        display(decreaseQuantity);
+
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(" " + number);
+    }
+
+
+    @TargetApi(24)
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
