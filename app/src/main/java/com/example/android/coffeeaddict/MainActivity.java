0afb72a4ca8e049f3ef860e6c498288bf9984843
daniel-enere <daniel.enere@gmail.com>
2017-10-29 22:00:49 -0500

    converted local variable to global variable

--- app/src/main/java/com/example/android/coffeeaddict/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeaddict/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.lang.String;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    int Quantity = 0;
+//    int numCoffees = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +26,21 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numCoffees = 2;
-        display(numCoffees);
-        displayPrice(numCoffees*3);
+        display(Quantity);
+        displayPrice(Quantity*3);
     }
 
     public void increaseOrder(View view) {
-        int increaseQuantity = 3;
-        increaseQuantity = 4;
-        display(increaseQuantity);
+        Quantity += 1;
+        display(Quantity);
     }
 
     public void decreaseOrder(View view) {
-        int decreaseQuantity = 1;
-        decreaseQuantity = 0;
-        display(decreaseQuantity);
+        if (Quantity>0) {
+            Quantity -= 1;
+            display (Quantity);
+        }
+        System.out.println("Cannot be less than 0");
 
     }
 
